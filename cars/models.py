b2fd50951ec665f2224b172a03079fe604dd302f
WHusssain937 <wasim937@hotmail.com>
2020-09-13 10:58:46 +0000

    added Cars app and models

---
+++ cars/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=254)
+    
+    def __str__(self):
+        return self.brand_name
+
+
+class Car(models.Model):
+    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    make = models.CharField(max_length=254)
+    model = models.CharField(max_length=254)
+    year = models.IntegerField()
+    transmission = models.CharField(max_length=254, null=True, blank=True)
+    fuel_type = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.make
