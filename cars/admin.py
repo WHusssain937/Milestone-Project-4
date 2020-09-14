ccb1ea89ebd9e6b7fbf273dbc53ba9d73c97e75d
WHusssain937 <wasim937@hotmail.com>
2020-09-14 14:23:33 +0000

    fixed issue with car model, added field to car model and customized admin

--- cars/admin.py
+++ cars/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Car, Brand
 
 # Register your models here.
-admin.site.register(Car)
-admin.site.register(Brand)
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'brand',
+        'make',
+        'model',
+        'year',
+        'price',
+        'image'
+    )
+
+    ordering = ('brand',)
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = (
+        'brand_name',
+    )
+
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
