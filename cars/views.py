1e2a7e3fe0aa94de29b98974202c252cd2416ea7
WHusssain937 <wasim937@hotmail.com>
2020-09-16 17:48:06 +0000

    added search car functionality

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Car
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Car, Brand
 
 # Create your views here.
 
@@ -8,11 +10,23 @@ def all_cars(request):
     """ View will show all cars, including sorting and search queries """
 
     cars = Car.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('cars'))
+        
+        queries = Q(make__icontains=query) | Q(model__icontains=query)
+        cars = cars.filter(queries)
 
     context = {
         'cars': cars,
+        'search_term': query,
     }
-    
+
     return render(request, 'cars/cars.html', context)
 
 def car_detail(request, car_id):
