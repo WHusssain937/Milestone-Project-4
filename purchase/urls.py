70969b2f40393a766cb3c3e2f97256bf722147b7
WHusssain937 <wasim937@hotmail.com>
2020-09-27 19:48:07 +0000

    added car.id to purchase view and made minor adjustments in purchase.html

--- purchase/urls.py
+++ purchase/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.purchase, name='purchase'),
+    path('<car_id>', views.purchase, name='purchase'),
 ]
--- purchase/views.py
+++ purchase/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Car
 from .forms import Car_OrderForm
 
 
-def purchase(request):
+def purchase(request, car_id):
     """ View will render the product purchase page """
+    car = get_object_or_404(Car, pk=car_id)
+
     order_form = Car_OrderForm()
     template = 'purchase/purchase.html'
-    
+
     context = {
         'order_form': order_form,
+        'car': car,
     }
 
     return render(request, template, context)
