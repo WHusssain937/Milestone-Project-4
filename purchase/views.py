e2173c3ed97590697996b24edbfb9e75ca65f3c3
WHusssain937 <wasim937@hotmail.com>
2020-09-30 17:48:17 +0000

    added basic purchase functionality

--- purchase/views.py
+++ purchase/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.conf import settings
 
 from .models import Car
 from .forms import Car_OrderForm
 
+import stripe
+
 
 def purchase(request, car_id):
-    """ View will render the product purchase page """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     car = get_object_or_404(Car, pk=car_id)
 
-    order_form = Car_OrderForm()
-    template = 'purchase/purchase.html'
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=car.price,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
+    order_form = Car_OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    template = 'purchase/purchase.html'
     context = {
         'order_form': order_form,
         'car': car,
-        'stripe_public_key': 'pk_test_51HIwyCGiwXlMOYYApouICUxrODcKuUnRPz84uzPbdBySwnVnYa4mTD3XQM16a8tI6SJalulaBlkPDtnyFxpbypPf004pZAZaIU',
-        'client_secret': 'text client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
