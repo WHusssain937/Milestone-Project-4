3f1dfe1fc4cd2f96db91757ea9df9823736941ea
WHusssain937 <wasim937@hotmail.com>
2020-10-06 21:30:41 +0000

    Added purchase form data caching in payment intent

--- purchase/urls.py
+++ purchase/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('<car_id>', views.purchase, name='purchase'),
     path('purchase_success/<order_number>', views.purchase_success,
          name='purchase_success'),
+    path('cache_purchase_data/', views.cache_purchase_data,
+         name='cache_purchase_data'),
     path('wh/', webhook, name='webhook'),
 ]
--- purchase/views.py
+++ purchase/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -7,6 +8,22 @@ from cars.models import Car
 from .forms import Car_OrderForm
 
 import stripe
+
+
+@require_POST
+def cache_purchase_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'save_info': request.POST.get('save_info'),
+            'user': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def purchase(request, car_id):
