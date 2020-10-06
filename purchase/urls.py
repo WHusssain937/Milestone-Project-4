e0ffc1096d457459522792ed0c9f7f4cb7ae2fb2
WHusssain937 <wasim937@hotmail.com>
2020-10-06 16:09:40 +0000

    added class webhook handler and webhook view

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('<car_id>', views.purchase, name='purchase'),
     path('purchase_success/<order_number>', views.purchase_success,
          name='purchase_success'),
+    path('wh/', webhook, name='webhook'),
 ]
