62778645aad15fa5529d364c0c5823db0a518cc9
WHusssain937 <wasim937@hotmail.com>
2020-10-11 15:22:41 +0000

    updated allauth templates and tested register and login

--- profile/models.py
+++ profile/models.py
@@ -27,9 +27,9 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or Update User Account
+    Create or update the user profile
     """
     if created:
-        UserProfile.objects.created(user=instance)
-    # Existing users: just save the account
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
     instance.userprofile.save()
