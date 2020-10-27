3e800e093ff125e372c033bf827fcf45a9faed36
WHusssain937 <wasim937@hotmail.com>
2020-10-27 20:19:49 +0000

    added review search box functionality

--- review/views.py
+++ review/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Review
 
 # Create your views here.
@@ -9,9 +11,22 @@ def all_reviews(request):
      and sorting queries"""
 
     reviews = Review.objects.all()
+    query = None
+
+    if request.GET:
+        if 'r' in request.GET:
+            query = request.GET['r']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('all_reviews'))
+
+        queries = Q(make__icontains=query) | Q(model__icontains=query) | Q(year__icontains=query)
+
+        reviews = reviews.filter(queries)
 
     context = {
         'reviews': reviews,
+        'search_term': query,
     }
 
     return render(request, 'review/reviews.html', context)
