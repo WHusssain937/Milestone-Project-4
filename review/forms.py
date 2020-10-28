54b7a1d12a2c8504192e21fe9d6436c013264a1f
WHusssain937 <wasim937@hotmail.com>
2020-10-28 18:31:13 +0000

    Review Admin - Review Form added

---
+++ review/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .widgets import CustomClearableFileInput
+from .models import Review
+from cars.models import Brand
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+    
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        brands = Brand.objects.all()
+        brand_names = [(b.id, b.__str__()) for b in brands]
+
+        self.fields['brand'].choices = brand_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
