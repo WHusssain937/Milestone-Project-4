95f9fba1537e71725a6c2bc18a98b5d775e7d429
WHusssain937 <wasim937@hotmail.com>
2020-11-08 11:03:05 +0000

    deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn german-whips.wsgi:application
--- german_whips/settings.py
+++ german_whips/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'oeis-)gt2m)xckm9uwcj#r#b#yb#7szd4a)1g81k#*f0c*pf0!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['german-whips.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'german_whips.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
