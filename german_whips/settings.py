0d73dc2d1e5646a3c14d3ac61f4b5b868c7e6098
WHusssain937 <wasim937@hotmail.com>
2020-11-08 12:51:12 +0000

    Removed secret key and set debug

--- german_whips/settings.py
+++ german_whips/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oeis-)gt2m)xckm9uwcj#r#b#yb#7szd4a)1g81k#*f0c*pf0!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['german-whips.herokuapp.com', 'localhost']
 
